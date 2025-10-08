f6b4deb7167273ec6e22605ae79b79fd6f1dd7bc
Deepraj Baidya <deeprajbaidya@gmail.com>
2025-10-08 20:43:43 +0530

    🚀 (chore): project config

---
+++ lib/src/window_states.dart
@@ -0,0 +1,7 @@
+/// {@template window_states}
+/// Effortless Window State Transitions for Flutter Desktop
+/// {@endtemplate}
+class WindowStates {
+  /// {@macro window_states}
+  const WindowStates();
+}
---
+++ lib/window_states.dart
@@ -0,0 +1,4 @@
+/// Effortless Window State Transitions for Flutter Desktop
+library;
+
+export 'src/window_states.dart';
